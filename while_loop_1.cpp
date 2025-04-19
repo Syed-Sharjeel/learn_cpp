9bab9475973114f069362d4a5603bf44730f45a9
Syed Sharjeel <syedsharjeel321@gmail.com>
2025-04-20 00:03:15 +0500

    Update while_loop_1.cpp

--- while_loop_1.cpp
+++ while_loop_1.cpp
@@ -1,3 +1,22 @@
+/*
+Write a program that continuously asks user to enter an integer and displays the SUM of the current 
+input with all previous input. The program continuous to run until the SUM value is less than equal to 
+100. Use while() loop. 
+ 
+Sample Run: 
+Enter an integer: 12 [Enter] 
+Running Sum = 12 
+ 
+Enter an integer: 10 [Enter] 
+Running Sum = 22 
+ 
+Enter an integer:70 [Enter] 
+Running Sum = 92 
+. 
+. 
+. 
+Sum exceeds 100. Program terminated.
+*/
 #include<iostream>
 using namespace std;
 int main(void)
